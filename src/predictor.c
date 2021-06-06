2d334efaf2e8da8af3fed61777488e65967f9ac9
vaibhavtiwari33 <vk.tiwari@samsung.com>
2021-06-05 20:47:09 -0700

    Tournament implementation

--- src/predictor.c
+++ src/predictor.c
@@ -6,6 +6,7 @@
 //  described in the README                               //
 //========================================================//
 #include <stdio.h>
+#include <string.h>
 #include "predictor.h"
 
 //
@@ -38,10 +39,20 @@ int verbose;
 //
 
 //gshare:BHT, history
-uint32_t *gshare_BHT;  // gshare branch history table
-uint32_t global_history;  //global history
+uint8_t *gshare_BHT;  // gshare branch history table
 int gshare_BHT_index;
-uint32_t gshare_result;
+unsigned int global_history;  //global history
+unsigned int gshare_result;
+
+//tournament: local BHT, local PHT, global history, global BHT, selector
+uint8_t *local_BHT;  // tournament local BHT 
+uint32_t *local_PHT;  // tournament local PHT 
+uint8_t *global_BHT;  // tournament global BHT 
+uint8_t *selector;  // selector fot tournament
+uint8_t local_outcome; //result form local predict
+uint8_t global_outcome; //result form global predict
+int PHT_index;
+int global_BHT_index;
 
 
 //------------------------------------//
@@ -56,19 +67,70 @@ init_predictor()
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
-
+  global_history = 0;
   switch (bpType) {
     case STATIC:
-      return TAKEN;
+      break;
     case GSHARE:
-      global_history = 0;
-      gshare_BHT = malloc((1<<ghistoryBits)*sizeof(uint32_t));
-      memset(gshare_BHT, WN, sizeof(uint32_t)*(1<<ghistoryBits));
+      gshare_BHT = malloc((1 << ghistoryBits) * sizeof(uint8_t));
+      memset(gshare_BHT, WN, sizeof(uint8_t) * (1 << ghistoryBits));
       break;
     case TOURNAMENT:
+      local_BHT = malloc((1 << lhistoryBits) * sizeof(uint8_t));
+      local_PHT = malloc((1 << pcIndexBits) * sizeof(uint32_t));
+      global_BHT = malloc((1 << ghistoryBits) * sizeof(uint8_t));
+      selector = malloc((1 << ghistoryBits) * sizeof(uint8_t));
+      memset(local_BHT, WN, sizeof(uint8_t) * (1 << lhistoryBits));
+      memset(local_PHT, 0, sizeof(uint32_t) * (1 << pcIndexBits));
+      memset(global_BHT, WN, sizeof(uint8_t) * (1 << ghistoryBits));
+      memset(selector, WN, sizeof(uint8_t) * (1 << ghistoryBits));
+      break;
     case CUSTOM:
     default:
       break;
+  }
+}
+
+unsigned int gshare_prediction(uint32_t pc) {
+  //XOR the program counter with global history and only keep ghistory amount of bits
+  gshare_BHT_index = (pc ^ global_history) & ((1 << ghistoryBits) - 1);
+
+  if (gshare_BHT[gshare_BHT_index] == WN || gshare_BHT[gshare_BHT_index] == SN) {
+    gshare_result = NOTTAKEN;
+  }
+  else {
+    gshare_result = TAKEN;
+  }
+  
+  return gshare_result;
+}
+
+
+uint8_t tournament_prediction(uint32_t pc) {
+  PHT_index = pc & ((1 << pcIndexBits) - 1);
+  
+  uint8_t local_prediction = local_BHT[local_PHT[PHT_index]];
+  if(local_prediction == WT || local_prediction == ST) {
+    local_outcome = TAKEN;
+  } else {
+    local_outcome = NOTTAKEN;
+  }
+
+  global_BHT_index = global_history & ((1 << ghistoryBits) - 1);
+  uint8_t global_prediction = global_BHT[global_BHT_index];
+  if(global_prediction == WT || global_prediction == ST) {
+    global_outcome = TAKEN;
+  } else {
+    global_outcome = NOTTAKEN;
+  }
+  
+  // predictor's selection
+  // if selector[global_BHT_index] == WT(2) or ST(3) => choose local predictor
+  // else choose global predictor
+  if (selector[global_BHT_index] == WT || selector[global_BHT_index] == ST) {
+    return local_outcome;
+  } else {
+    return global_outcome;
   }
 }
 
@@ -79,24 +141,14 @@ init_predictor()
 uint8_t
 make_prediction(uint32_t pc)
 {
-
   // Make a prediction based on the bpType (Branch Predictor type)
   switch (bpType) {
     case STATIC:
       return TAKEN;
     case GSHARE:
-      //XOR the program counter with global history and only keep ghistory amount of bits
-      gshare_BHT_index = (pc ^ global_history) & ((1 << ghistoryBits) - 1);
-
-      if (gshare_BHT[gshare_BHT_index] == WN || gshare_BHT[gshare_BHT_index] == SN) {
-        gshare_result = NOTTAKEN;
-      }
-      else {
-        gshare_result = TAKEN;
-      }
-      
-      return gshare_result;
+      return gshare_prediction(pc);
     case TOURNAMENT:
+      return tournament_prediction(pc);
     case CUSTOM:
     default:
       break;
@@ -120,11 +172,58 @@ train_gshare(uint32_t pc, uint8_t outcome) {
         gshare_BHT[BHTindex]++;
     } 
 
-    global_history <<= 1;                           //Right shift global history
-    global_history  &= ((1 << ghistoryBits) - 1);   //Only keep ghistoryBits amount of history
-    global_history |= outcome;                       //Fill the new last bit of global history with outcome
+    //Right shift global history
+    //Only keep ghistoryBits amount of history
+    //Fill the new last bit of global history with outcome
+    global_history = (((global_history << 1) | (outcome)) & ((1 << ghistoryBits) - 1));
 }
 
+void 
+update_tournament_predictors(uint32_t pc, uint8_t outcome) {
+    PHT_index = pc & ((1 << pcIndexBits) - 1);
+
+    // Update local predictor
+    int local_BHT_index = local_PHT[PHT_index];
+    if (outcome == NOTTAKEN && local_BHT[local_BHT_index] != SN) {
+      local_BHT[local_BHT_index]--;
+    } else if (outcome == TAKEN && local_BHT[local_BHT_index] != ST) {
+      local_BHT[local_BHT_index]++;
+    } 
+
+    // Update global predictor
+    global_BHT_index = global_history & ((1 << ghistoryBits) - 1);
+    if (outcome == NOTTAKEN && global_BHT[global_BHT_index] != SN) {
+      global_BHT[global_BHT_index]--;
+    } else if (outcome == TAKEN && global_BHT[global_BHT_index] != ST) {
+      global_BHT[global_BHT_index]++;
+    }
+}
+
+void 
+update_tournament_selector(uint8_t outcome) {
+    global_BHT_index = global_history & ((1 << ghistoryBits) - 1);
+
+    if (local_outcome != outcome && selector[global_BHT_index] != SN) {
+      // If local outcome was wrong && selector isn't saturated then shift the selector towards global predictor
+      selector[global_BHT_index]--;
+    } else if (local_outcome == outcome && selector[global_BHT_index] != ST) {
+      // else shift the selector towards local predictor if selector isn't saturated 
+      selector[global_BHT_index]++;    
+    }
+}
+
+void 
+train_tournament(uint32_t pc, uint8_t outcome) {
+    if (global_outcome != local_outcome) {
+        update_tournament_selector(outcome);
+    }
+    update_tournament_predictors(pc, outcome);
+
+    global_history = (((global_history << 1) | (outcome)) & ((1 << ghistoryBits) - 1));
+
+    int PHT_index = pc & ((1 << pcIndexBits) - 1);
+    local_PHT[PHT_index] = (((local_PHT[PHT_index] << 1) | (outcome)) & ((1 << lhistoryBits) - 1));
+}
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
@@ -140,7 +239,7 @@ train_predictor(uint32_t pc, uint8_t outcome)
       train_gshare(pc, outcome);
       break;
     case TOURNAMENT:
-      //tournament_update(pc, outcome);
+      train_tournament(pc, outcome);
       break;
     case CUSTOM:
       //per_train(pc, outcome);
